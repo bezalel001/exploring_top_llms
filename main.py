bdc37e57c09140702e1155d34e11ac1484bf5300
Michael Ogu <mickeybezalel@gmail.com>
2025-11-08 22:30:17 +0100

    feat: prompt templates

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
-def main():
-    print("Hello from exploring-top-llms!")
+from langchain_core.prompts import ChatPromptTemplate
 
+#  setup chat prompt template
+prompt_template = ChatPromptTemplate.from_messages([
+    ("system", "You're an AI assistant that translates English into another language."),
+    ("user", "Translate this sentence: '{input}' into {target_language}.")
+])
 
-if __name__ == "__main__":
-    main()
+# Invoke prompt template with variables
+
+print(prompt_template.invoke({"input": "I love programming.", "target_language": "Russian"}))
+
+print('hello')
